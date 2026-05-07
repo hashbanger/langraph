09583b201370a9831c6e974d34c7955529d940c9
hashbanger <prashant.brahmbhatt32@outlook.com>
2026-05-07 09:48:22 +0530

    refactor chatbot response handling for improved message formatting

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -19,9 +19,8 @@ if user_input:
     with st.chat_message("user"):
         st.text(user_input)
 
-    print(user_input)
-    response = chatbot.invoke({'messages': HumanMessage(content=user_input)}, config=CONFIG)
-    print(response)
+    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
+
     ai_message = response["messages"][-1].content
 
     st.session_state["message_history"].append({"role": "assistant", "content": ai_message})
